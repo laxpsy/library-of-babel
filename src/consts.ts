1d41d59e111fb65c672d26220ced45184a2dffe8
laxpsy <gujralanshg@gmail.com>
2024-06-18 01:28:32 +0530

    feat: 404 added w scrambling-text. fix: typo

--- src/consts.ts
+++ src/consts.ts
@@ -9,3 +9,33 @@ export const MAILTO_LINK = "mailto:gujralanshg@gmail.com";
 export const LINKEDIN_URL = "https://www.linkedin.com/in/ansh-gujral-9b2a7a23b/";
 export const LIBRARIAN = "Ansh Gujral";
 export const REPO_LINK = "https://github.com/laxpsy/Library-Of-Babel";
+export const PHRASES = [
+  "infinite paths to be taken",
+  "infinite values to be found",
+  "infinite possibilities to be explored",
+  "infinite stories to be told",
+  "infinite worlds to be created",
+  "infinite dreams to be dreamt",
+  "infinite lives to be lived",
+  "infinite people to be loved",
+  "infinite joys to be experienced",
+  "infinite books to be read",
+  "infinite places to be seen",
+  "infinite routes to be discovered",
+  "infinite sorrows to be embraced",
+  "infinite silences to be heard",
+  "infinite songs to be played",
+  "infinite dances to be danced",
+  "infinite masterpieces to be created",
+  "infinite words to be written",
+  "infinite thoughts to be thought",
+  "infinite feelings to be felt",
+  "infinite sensations to be sensed",
+  "infinite experiences to be experienced",
+  "infinite moments to be lived",
+  "infinite breaths to be taken",
+  "infinite heartbeats to be felt",
+  "infinite connections to be made",
+  "infinite relationships to be formed",
+  "infinite memories to be made"
+];
